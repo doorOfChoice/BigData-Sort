fb1f0312b9f05bf8a7ad78869c5ab949608db1d3
dawndevil <kevinkiller766@gmail.com>
2018-04-01 17:27:54 +0800

    update : 更新注释

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Generator g = new Generator("./sort/", 10000, 30);
-        ExternalSort e = new ExternalSort(g, "result.txt");
+        //随机文件生成的路径
+        String generatePath = "sort/";
+        //生成文件的数量
+        int count = 30;
+        //每个文件的数字数量
+        int per = 10000;
+        //生成最终文件名称
+        String resultFile = "result.txt";
+
+        Generator g = new Generator(generatePath, per, count);
+        ExternalSort e = new ExternalSort(g, resultFile);
         e.start();
     }
 }
